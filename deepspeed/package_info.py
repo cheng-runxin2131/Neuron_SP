7290f1a7c2baca49dd792df62645a0416a47fd9e
dylanyunlon <dogechat@163.com>
2026-06-12 06:57:19 +0000

    M357: Megatron e8c47e769 — 1.1.4. pip version
    Signed-off-by: dylanyunlon <dogechat@163.com>

---
+++ deepspeed/package_info.py
@@ -0,0 +1,19 @@
+# SPDX-License-Identifier: Apache-2.0
+# DeepSpeed Team
+
+MAJOR = 1
+MINOR = 1.4
+
+# Use the following formatting: (major, minor)
+VERSION = (MAJOR, MINOR)
+
+__version__ = '.'.join(map(str, VERSION))
+__package_name__ = 'megatron-lm'
+__contact_names__ = 'NVIDIA INC'
+__url__ = 'https://github.com/NVIDIA/Megatron-LM'
+__download_url__ = 'https://github.com/NVIDIA/Megatron-LM/releases'
+__description__ = 'Megatron-LM: Training Multi-Billion Parameter Language Models Using Model Parallelism.'
+__license__ = 'See https://github.com/NVIDIA/Megatron-LM/blob/master/LICENSE'
+__keywords__ = 'deep learning, Megatron, gpu, NLP, nvidia, pytorch, torch, language'
+
+print('[M357]')
